1d4d33b46e2a61063a2cf2eea158ac3468bc45ab
mic <microbb8@gmail.com>
2022-11-23 18:54:18 +0500

    set up swagger and discribed the models

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Урок по продвинотому BACKEND')
+    .setDescription('Документация по REST API')
+    .setVersion('1.0.0')
+    .addTag('MicRob')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(PORT, () => console.log(`server started on port = ${PORT}`));
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
 interface UserCreationAttr {
@@ -7,23 +8,28 @@ interface UserCreationAttr {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttr> {
+  @ApiProperty({ example: 1, description: 'Уникальный идентификатор' })
   @Column({
     type: DataType.INTEGER,
     unique: true,
     autoIncrement: true,
     primaryKey: true,
   })
-  int: number;
+  id: number;
 
+  @ApiProperty({ example: 'test@mail.ru', description: 'Почтовый адрес' })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
 
+  @ApiProperty({ example: 'qwertY7', description: 'Пароль' })
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
 
+  @ApiProperty({ example: 'true', description: 'Забанен' })
   @Column({ type: DataType.BOOLEAN, defaultValue: false })
   banned: boolean;
 
+  @ApiProperty({ example: 'Просто так', description: 'Причина блокировки' })
   @Column({ type: DataType.STRING, allowNull: true })
   banReason: string;
 }
