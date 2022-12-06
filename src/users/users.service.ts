d8c3867aedb1c0f90efed88a5bdda4ca741702a1
Mic <microbb8@gmail.com>
2022-12-06 22:07:23 +0500

    вылезла какая то проблема при добавлении роли по умолчанию

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { RolesService } from 'src/roles/roles.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User) private userRepository: typeof User) { }
+  constructor(
+    @InjectModel(User) private userRepository: typeof User,
+    private roleService: RolesService,
+  ) { }
 
   async createUser(dto: CreateUserDto) {
-    return await this.userRepository.create(dto);
+    const user = await this.userRepository.create(dto);
+    const role = await this.roleService.getRoleByValue('USER');
+    // какая то ошибка ниже, пока не разобрался, оставил как есть
+    await user.$set('roles', [role.id]);
+    return user;
   }
 
   async getAllUsers() {
-    return await this.userRepository.findAll();
+    return await this.userRepository.findAll({ include: { all: true } });
   }
 }
